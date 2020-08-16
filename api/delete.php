51f778880c7e67c2d32832c138c5f6f53ca2c2ba
Giuliano Criscuoli <giulianodias450@gmail.com>
2020-08-16 08:34:20 -0300

    Comentários e atualização do README.md

--- api/delete.php
+++ api/delete.php
@@ -2,20 +2,29 @@
 
 require('../config.php');
 
+// passando o método para  avariável
+
 $method = strtolower($_SERVER['REQUEST_METHOD']);
 
+// verificando se o método é delete
 
 if($method === 'DELETE' || $method === 'delete') {
+
+    // passando as informações do input para a variável $input
 
     parse_str(file_get_contents("php://input"), $input);
 
     $id = filter_var($input['id']) ?? null;
+
+    // verificando se existe o id retornado
 
     if($id) {
 
         $sql = $pdo->prepare("SELECT * FROM notes WHERE id = :id");
         $sql->bindValue(":id", $id);
         $sql->execute();
+
+        // se existir, exclua o registro
 
         if($sql->rowCount() > 0) {
 
